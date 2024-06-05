d82452fa05ccb04f81a9f6a968b8975d1df62ccd
thehasanovv <hasanov.e95@gmail.com>
2024-06-05 17:24:21 +0400

    task added

--- src/ClassTask.java
+++ src/ClassTask.java
@@ -1,5 +1,10 @@
-
 public class ClassTask {
     public static void main(String[] args) {
+        for (int i = 7; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
